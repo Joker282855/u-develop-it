dbf256bb7fc95a4ead412224e6614fdcbd212096
unknown <jtjackson325@gmail.com>
2021-11-10 18:44:25 -0700

    got some of the queries tested

--- server.js
+++ server.js
@@ -21,9 +21,20 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, rows) => {
+    if (err) {
+        console.log(err);
+    }
     console.log(rows);
-})
+});
+
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
