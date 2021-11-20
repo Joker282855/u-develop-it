62cf26beda07fdc2ba3c4369fe3e0f1704d7c123
unknown <jtjackson325@gmail.com>
2021-11-19 18:24:51 -0700

    got the routes set up in the folder for the candidate routes

--- routes/apiRoutes/candidateRoutes.js
+++ routes/apiRoutes/candidateRoutes.js
@@ -2,3 +2,25 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
+
+router.get('/candidates', (req, res) => {
+
+});
+
+router.get('/candidate/:id', (req, res) => {
+
+});
+
+router.post('/candidate', ({ body }, res) => {
+
+});
+
+router.put('/candidate/:id', (req, res) => {
+
+});
+
+router.delete('/candidate/:id', (req, res) => {
+
+});
+
+module.exports = router;
