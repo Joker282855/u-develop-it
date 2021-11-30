80bf3e1e700f8eef48a20bd66810b13035828f40
unknown <jtjackson325@gmail.com>
2021-11-30 16:33:59 -0700

    got the rest of the post route for the votes table created

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -14,7 +14,18 @@ router.post('/vote', ({ body }, res) => {
     const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?,?)`;
     const params = [body.voter_id, body.candidate_id];
 
-    
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            changes: resultaffectedRows
+        });
+    });
+
 });
 
 module.exports = router;
