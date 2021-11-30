e60e6f4db6dbc630a58cd21cc44b3df7d1e83228
unknown <jtjackson325@gmail.com>
2021-11-29 18:59:04 -0700

    creating the post route for the voters in the route

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -4,7 +4,7 @@ const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
 router.get('/voters', (req, res) => {
-    const sql = `SELECT * FROM voters`;
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -16,5 +16,31 @@ router.get('/voters', (req, res) => {
         })
     })
 });
+
+// Get a single voter
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params  = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// Create another voter with the original values from the table
+router.post('/voter', ({ body }, res) => {
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.query(sql, params)
+});
+
 
 module.exports = router;
