121ef751d2f4e6ff8447fdcab0ec3fab3d92ad5d
unknown <jtjackson325@gmail.com>
2021-11-30 12:27:35 -0700

    got the constraints for the candidates table to enable using the left join method in the future

--- db/schema.sql
+++ db/schema.sql
@@ -29,8 +29,11 @@ CREATE TABLE voters (
 CREATE TABLE votes (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     voter_id INTEGER NOT NULL,
-    cadidate_id INTEGER NOT NULL,
+    candidate_id INTEGER NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uc_voter UNIQUE (voter_id),
+    CONSTRAINT fk_voter FOREIGN KEY voter_id REFERENCES voters(id) ON DELETE CASCADE,
+    CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
 
 
