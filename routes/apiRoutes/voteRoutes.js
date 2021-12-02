1c75f9933e52394a9470a45e37e1a873ff070575
unknown <jtjackson325@gmail.com>
2021-12-01 20:32:10 -0700

    got a few of the gramatical mistakes worked out

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
-const inputCheck = require('../../utilsinputCheck');
+const inputCheck = require('../../utils/inputCheck');
 
 // the post route for the voters
 router.post('/vote', ({ body }, res) => {
@@ -22,30 +22,30 @@ router.post('/vote', ({ body }, res) => {
         res.json({
             message: 'success',
             data: body,
-            changes: resultaffectedRows
+            changes: result.affectedRows
         });
     });
 
 });
 
 // route for the get vote totals
-router.get('/votes', (err, res) => {
+router.get('/votes', (req, res) => {
     const sql =`SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
                 FROM votes
                 LEFT JOIN candidates ON votes.candidate_id = candidates.id
                 LEFT JOIN parties ON candidates.party_id = parties.id
-                GROUP BY candidate_id ORDER BY count DESC`
+                GROUP BY candidate_id ORDER BY count DESC`;
 
-    db.query(sql, (err, row) => {
+    db.query(sql, (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
         }
         res.json({
             message: 'success',
-            data: row
-        });
-    });
-})
+            data: rows
+        })
+    })
+});
 
 module.exports = router;
