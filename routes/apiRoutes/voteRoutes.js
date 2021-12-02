def61274e8d9e3fe8652914d7bd7f33e32ed0936
unknown <jtjackson325@gmail.com>
2021-12-01 20:19:08 -0700

    got the get route for the vote totals from the vote table

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -28,4 +28,24 @@ router.post('/vote', ({ body }, res) => {
 
 });
 
+// route for the get vote totals
+router.get('/votes', (err, res) => {
+    const sql =`SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
+                FROM votes
+                LEFT JOIN candidates ON votes.candidate_id = candidates.id
+                LEFT JOIN parties ON candidates.party_id = parties.id
+                GROUP BY candidate_id ORDER BY count DESC`
+
+    db.query(sql, (err, row) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+})
+
 module.exports = router;
