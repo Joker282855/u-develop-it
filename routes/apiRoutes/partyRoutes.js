d2b3626e9c1b48b0ea84abd4068ea9a40864b937
unknown <jtjackson325@gmail.com>
2021-12-03 20:49:42 -0700

    got an error fixed with the get route for the parties table

--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -2,18 +2,16 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
 
-router.get('/parties', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    
-    db.query(sql, params, (err, row) => {
+router.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
         if (err) {
-            res.status(400).json({ error: err.message });
+            res.status(500).json({ error: err.message });
             return;
         }
         res.json({
             message: 'success',
-            data: row
+            data: rows
         });
     });
 });
